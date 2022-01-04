19bc496558e54f5617a3d6290be3f151ec9a7ca8
Barbara Liao <barbara.liao3@gmail.com>
2022-01-03 21:34:42 -0800

    Completed longest common prefix challenge.

--- leet-code.js
+++ leet-code.js
@@ -48,3 +48,15 @@ var isPalindrome = function (x) {
   }
   return true;
 };
+
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return '';
+  let r = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return r;
+    }
+    r += strs[0][i];
+  }
+  return r;
+};
