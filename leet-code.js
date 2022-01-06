542e80c4ed6bfb0ca2b8b5e81ef2386b94dbda44
Barbara Liao <barbara.liao3@gmail.com>
2022-01-05 21:40:47 -0800

    completed is valid challenge.

--- leet-code.js
+++ leet-code.js
@@ -60,3 +60,20 @@ var longestCommonPrefix = function (strs) {
   }
   return r;
 };
+
+function isValid(s) {
+  const left = [];
+  const legend = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      left.push(s[i]);
+    } else if (legend[left.pop()] !== s[i]) {
+      return false;
+    }
+  }
+  return left.length ? 0 : 1;
+};
